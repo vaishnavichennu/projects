8951cc6e9209e54da32a27249079db3535b80aa5
Chennu Vaishnavi Priya <65950204+vaishnavichennu@users.noreply.github.com>
2020-05-26 15:34:27 +0530

    Add files via upload

---
+++ age_calc.py
@@ -0,0 +1,17 @@
+
+def calc_age():
+ x=int(input("enter the year of the candidate="))
+ y=int(input("enter the current year="))
+ print("D.O.Y=",x)
+ print("current yr=",y)
+ age=y-x
+ print("age of the candiate=",age)
+ return age
+
+calc_age()
+'''
+if age>=18:
+ print(age,"eligible to vote")
+else:
+ print("not eliginle")
+'''
