63f295e9a7b637f1b8bba2eb80d0d5d2c6afc6c6
Chennu Vaishnavi Priya <65950204+vaishnavichennu@users.noreply.github.com>
2020-06-04 10:11:09 +0530

    Add files via upload

---
+++ constructor_without_args.py
@@ -0,0 +1,9 @@
+class Bank:
+ def __init__(self):
+  self.accNo=21344567768;
+  self.bal=200000;
+ def show(self):
+  print("account no=",self.accNo);
+  print("balnce=",self.bal);
+vaishnavi=Bank();
+vaishnavi.show();
